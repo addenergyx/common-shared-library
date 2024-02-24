a78b4bc4b295a553ebfc9ddd222dccc78aef7299
David <david-adeniji@hotmail.co.uk>
2024-02-24 09:24:39 +0000

    Fixed module name

--- common_shared_library/__init__.py
+++ common_shared_library/__init__.py
@@ -1,3 +1,3 @@
 from .captcha_bypass import CaptchaBypass
-from .push_notifications import notification_manager
+from .notification_manager import NotificationManager
 from .aws_manager import AWSConnector
