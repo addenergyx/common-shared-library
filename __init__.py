7a9a2faea82b86bcdfccf6959e3ac2fc884d9c83
David <david-adeniji@hotmail.co.uk>
2024-02-24 09:02:21 +0000

    Streamline importing by setting in init

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .captcha_bypass import CaptchaBypass
-from .push_notifications import notification_manager
-from .aws_manager import AWSConnector
---
+++ common_shared_library/__init__.py
@@ -0,0 +1,3 @@
+from .captcha_bypass import CaptchaBypass
+from .push_notifications import notification_manager
+from .aws_manager import AWSConnector
