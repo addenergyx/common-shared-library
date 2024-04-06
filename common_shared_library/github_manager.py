2b0b9f61206e4cc60fae8992aea5b90d504c4756
David <david-adeniji@hotmail.co.uk>
2024-04-06 11:42:14 +0100

    github class for csv access

---
+++ common_shared_library/github_manager.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import requests, io
+
+
+class GithubHandler:
+    def __init__(self, user: str = os.getenv('GITHUB_USER'),
+                 access_token: str = os.getenv('GITHUB_PERSONAL_ACCESS_TOKEN')) -> None:
+        self.user = user
+        self.access_token = access_token
+
+    def download_csv(self, raw_csv_url: str) -> pd.DataFrame:
+        github_session = requests.Session()
+        github_session.auth = (self.user, self.access_token)
+
+        download = github_session.get(raw_csv_url).content
+        return pd.read_csv(io.StringIO(download.decode('utf-8')), on_bad_lines='skip')
